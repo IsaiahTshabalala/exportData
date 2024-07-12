0f6c8e0e6663665ebb82b9d5b9ccd54d19a2394c
Isaiah Tshabalala <lakeside.junction.1984@gmail.com>
2024-07-12 11:34:27 +0200

    Add env variable LOCAL and option as to whether to import to the local emulator or Firestore.

--- index3.js
+++ index3.js
@@ -7,23 +7,31 @@
  * Date         Dev  Version  Description
  * 2024/01/20   ITA  1.00     Dump the provinces3.json file to Firestore.
  * 2024/07/11   ITA  1.01     Install and use dotenv to access the environment variables.
+ * 2024/07/12   ITA  1.02     Add env variable LOCAL and option as to whether to export to the local emulator or Firestore.
 */
 const { initializeFirebaseApp, restore } = require('firestore-export-import');
 require('dotenv').config();
 const serviceAccount = require(process.env.SERVICE_ACCOUNT_FILE);
 
 // If you want to pass settings for firestore, you can add to the options parameters
-const options = {
-    firestore: {
-        host: 'localhost:8080',
-        ssl: false,
-    },
+let options = null;
+if (process.env.LOCAL.toUpperCase() === 'TRUE') { // Use the local emulator.
+    options = {
+            firestore: {
+                host: 'localhost:8080',
+                ssl: false,
+            },
+        };
 }
 
 // Initiate Firebase App
 const appName = '[DEFAULT]';
 
-const firestore = initializeFirebaseApp(serviceAccount, appName, options);
+let firestore;
+if (options === null)
+    firestore = initializeFirebaseApp(serviceAccount, appName);
+else
+    firestore = initializeFirebaseApp(serviceAccount, appName, options);
 
 restore(firestore, './provinces3.json')
     .then(result=> {
